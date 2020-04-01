05288b086e2883e4f33c39758a5e26a293463c89
Hunter Danielson <hunterleedanielson@gmail.com>
2020-04-01 15:58:39 -0700

    make reset functionality work

--- app.js
+++ app.js
@@ -3,15 +3,19 @@ import { getRandomThrow } from './get-random-throw.js';
 
 // get things from the DOM
 const playButton = document.getElementById('play-button');
-//const resetButton = document.getElementById('reset-button');
+const resetButton = document.getElementById('reset-button');
 const winDiv = document.getElementById('wins');
 const loseDiv = document.getElementById('loses');
 const drawDiv = document.getElementById('draws');
 const totalDiv = document.getElementById('total');
+const computerChoiceDiv = document.getElementById('computer-choice');
+const computerChoiceHeading = document.getElementById('computer-choice-heading');
+const timesReset = document.getElementById('times-reset');
 
 let wins = 0;
 let draws = 0;
 let total = 0;
+let resetButtonCount = 0;
 
 playButton.addEventListener('click', () => {
 
@@ -25,6 +29,8 @@ playButton.addEventListener('click', () => {
     // computer choice
     let computerChoice = getRandomThrow();
     console.log('computerChoice', computerChoice);
+    
+    
     
 
     // figure out if we won
@@ -44,5 +50,29 @@ playButton.addEventListener('click', () => {
     loseDiv.textContent = loses;
     drawDiv.textContent = draws;
     totalDiv.textContent = total;
+    // only shows up if game has been played
+    if (total > 0) {
+        computerChoiceHeading.style.visibility = 'visible';
+        computerChoiceDiv.textContent = computerChoice;
+    }
+    
+});
 
+resetButton.addEventListener('click', () => {
+    // reset counters
+    wins = 0;
+    draws = 0;
+    total = 0;
+    
+    // clear the text from the page
+    winDiv.textContent = '';
+    loseDiv.textContent = '';
+    drawDiv.textContent = '';
+    totalDiv.textContent = '';
+    computerChoiceHeading.style.visibility = 'hidden';
+    computerChoiceDiv.textContent = '';
+
+    // track how many times reset button pushed
+    resetButtonCount++;
+    timesReset.textContent = resetButtonCount;
 });
