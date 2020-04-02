5e08717335c2c5fe20b5b35e21191dc0e429c8e3
Lucia Brammer <brammerl96@gmail.com>
2020-04-02 15:04:18 -0700

    updating CSS and cleaning up some code

--- get-random-throw-LS.js
+++ get-random-throw-LS.js
@@ -2,10 +2,9 @@
 export function getRandomThrowLS() {
     // generate random choice for computer 
     let number = Math.floor(Math.random() * 5); 
-    // convert that number into a word
     
+    // convert that number into a word
     let choiceStack = ['rock', 'scissors', 'lizard', 'paper', 'spock'];
     let computerChoice = choiceStack[number];
     return computerChoice;
-
 }
